c9925ea84182d395fbb5c52de86a6fca975c24ba
Lai-Star <tsangyu.lai0920@gmail.com>
2024-10-15 09:16:26 +0900

    initial commit

---
+++ src/data/skillData.js
@@ -0,0 +1,86 @@
+export const skillData = [
+  {
+    id: crypto.randomUUID(),
+    technology: 'JavaScript/TypeScript',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'PHP',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Python/Django',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Go',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Ruby/Ruby On Rails',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Gin',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Laravel',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Symfony',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Node.js',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Nest.js',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'React/Next.js',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Vue/Nuxt.js',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Angular',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Android/IOS(React-Native, Swift)',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'MySQL',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'PostgreSQL',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Oracle',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'MongoDB',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'ArangoDB',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Redis',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'AWS',
+  },
+];
